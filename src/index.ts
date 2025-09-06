dff6fd57c891a9e3a99fa63beda15b29e6335852
haoziqaq <357229046@qq.com>
2025-09-06 17:11:09 +0800

    feat: refactor rulerFactory to improve type inference and extend context handling

--- src/index.ts
+++ src/index.ts
@@ -59,14 +59,11 @@ export type RulerContext<R, P, E> = {
   shouldToUpperCase: boolean
 } & E
 
-export function rulerFactory<
-  R,
-  P = R,
-  E extends (ctx: RulerContext<R, P, {}>) => Record<string, (...args: any[]) => any> = (
-    ctx: RulerContext<R, P, {}>,
-  ) => Record<string, (...args: any[]) => any>,
->(generator: RulerFactoryGenerator<R, P>, extend?: E) {
-  return function ruler(): RulerContext<R, P, ReturnType<E>> {
+export function rulerFactory<R, P = R, E extends Record<string, (...args: any[]) => any> = {}>(
+  generator: RulerFactoryGenerator<R, P>,
+  extend?: (ctx: RulerContext<R, P, {}>) => E,
+) {
+  return function ruler(): RulerContext<R, P, E> {
     const rules: R[] = []
 
     const _ctx: RulerContext<R, P, {}> = {
@@ -120,9 +117,9 @@ export function rulerFactory<
       shouldToUpperCase: false,
     }
 
-    const extended = (extend?.(_ctx) ?? {}) as ReturnType<E>
+    const extended = (extend?.(_ctx) ?? {}) as E
 
-    const ctx = { ..._ctx, ...extended } as RulerContext<R, P, ReturnType<E>>
+    const ctx = { ..._ctx, ...extended } as RulerContext<R, P, E>
 
     function string(message?: RulerFactoryMessage, params?: P) {
       ctx.type = 'string'
