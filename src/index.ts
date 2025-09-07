6338cb7c392689b03018fbe7f0fc23f12b451961
haoziqaq <357229046@qq.com>
2025-09-07 20:49:22 +0800

    types: optimize types

--- src/index.ts
+++ src/index.ts
@@ -8,56 +8,60 @@ export type RulerFactoryValidator = (value: any) => undefined | Error
 
 export type RulerFactoryGenerator<R, P> = (validator: RulerFactoryValidator, params?: P) => R
 
-export type RulerContext<R, P, E> = {
-  // helpers
-  rules: R[]
-  addRule: (validator: RulerFactoryValidator, params?: P) => RulerContext<R, P, E>
-  generator: RulerFactoryGenerator<R, P>
-  getMessage: (message: RulerFactoryMessage) => string
-  // type
-  type: string
-  string: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  number: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  array: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  boolean: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  object: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  symbol: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  bigint: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  null: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  undefined: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  true: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  false: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  // validation
-  length: (v: number, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  min: (v: number | bigint, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  max: (v: number | bigint, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  regex: (v: RegExp, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  required: (message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  startsWith: (v: string, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  endsWith: (v: string, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  includes: (v: string, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  uppercase: (message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  lowercase: (message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  email: (message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  gt: (v: number | bigint, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  gte: (v: number | bigint, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  lt: (v: number | bigint, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  lte: (v: number | bigint, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  positive: (message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  negative: (message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  is: (v: (value: any) => boolean, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  not: (v: (value: any) => boolean, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
-  done: () => R[]
-  // transform
-  transformer: (value: any) => any
-  transform: (v: (value: any) => any) => void
-  trim: () => RulerContext<R, P, E>
-  shouldTrim: boolean
-  toLowerCase: () => RulerContext<R, P, E>
-  shouldToLowerCase: boolean
-  toUpperCase: () => RulerContext<R, P, E>
-  shouldToUpperCase: boolean
-} & E
+export type RulerContext<R, P, E> = Omit<
+  {
+    // helpers
+    rules: R[]
+    addRule: (validator: RulerFactoryValidator, params?: P) => RulerContext<R, P, E>
+    generator: RulerFactoryGenerator<R, P>
+    getMessage: (message: RulerFactoryMessage) => string
+    // type
+    type: string
+    string: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    number: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    array: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    boolean: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    object: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    symbol: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    bigint: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    null: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    undefined: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    true: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    false: (message?: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    // validation
+    length: (v: number, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    min: (v: number | bigint, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    max: (v: number | bigint, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    regex: (v: RegExp, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    required: (message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    startsWith: (v: string, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    endsWith: (v: string, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    includes: (v: string, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    uppercase: (message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    lowercase: (message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    email: (message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    gt: (v: number | bigint, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    gte: (v: number | bigint, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    lt: (v: number | bigint, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    lte: (v: number | bigint, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    positive: (message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    negative: (message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    is: (v: (value: any) => boolean, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    not: (v: (value: any) => boolean, message: RulerFactoryMessage, params?: P) => RulerContext<R, P, E>
+    done: () => R[]
+    // transform
+    transformer: (value: any) => any
+    transform: (v: (value: any) => any) => void
+    trim: () => RulerContext<R, P, E>
+    shouldTrim: boolean
+    toLowerCase: () => RulerContext<R, P, E>
+    shouldToLowerCase: boolean
+    toUpperCase: () => RulerContext<R, P, E>
+    shouldToUpperCase: boolean
+  },
+  keyof E
+> &
+  E
 
 export function rulerFactory<R, P = R, E extends Record<string, any> = {}>(
   generator: RulerFactoryGenerator<R, P>,
@@ -66,7 +70,7 @@ export function rulerFactory<R, P = R, E extends Record<string, any> = {}>(
   return function ruler(): RulerContext<R, P, E> {
     const rules: R[] = []
 
-    const ctx = {
+    const ctx: RulerContext<R, P, {}> = {
       // helpers
       rules,
       addRule,
@@ -115,9 +119,9 @@ export function rulerFactory<R, P = R, E extends Record<string, any> = {}>(
       shouldToLowerCase: false,
       toUpperCase,
       shouldToUpperCase: false,
-    } as RulerContext<R, P, E>
+    }
 
-    Object.assign(ctx, (extend?.(ctx) ?? {}) as E)
+    Object.assign(ctx, (extend?.(ctx as any) ?? {}) as E)
 
     function string(message?: RulerFactoryMessage, params?: P) {
       ctx.type = 'string'
@@ -606,6 +610,6 @@ export function rulerFactory<R, P = R, E extends Record<string, any> = {}>(
       return isFunction(message) ? message() : message
     }
 
-    return ctx
+    return ctx as unknown as RulerContext<R, P, E>
   }
 }
